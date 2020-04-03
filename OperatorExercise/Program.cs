ff457699c52c3ee6d07ce300988133846d3579be
mvdoyle <mdoyle@truecoders.io>
2020-04-03 12:47:18 -0500

    created

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,30 +6,9 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            var a = 17;
-            var b = 4;
-
-            var div = a / b;
-            var mod = a % b;
-
-            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
-
-            //Allow user to input the radius
-            Console.WriteLine("What is the radius of your circle?");
-
-            double realRadius;
-            var radius = double.TryParse(Console.ReadLine(), out realRadius);
-
-            //Calculate area of the circle
-            var areaOfCircle = CalculateArea(realRadius);
-
-            Console.WriteLine($"The area of a circle with radius of {realRadius} is {areaOfCircle}");
+            
 
         }
 
-        public static double CalculateArea(double radius)
-        {
-            return Math.PI * (radius * radius);
-        }
     }
 }
